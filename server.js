4b423dc3829b2bdbe43148ee7441d286ca031641
Chidozie <chidough14@gmail.com>
2020-07-05 21:52:12 +0200

    initial commit

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ io.on('connection', socket => {
     })
 })
 
-const PORT = 3000 || process.env.PORT;
+//const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
 //server.listen(PORT, '10.5.16.246');
